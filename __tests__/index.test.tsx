4aa566ccc2bf1be18d996ad8d497074d31d5ad43
inoway46 <inoueyuya416@gmail.com>
2023-08-11 11:00:00 +0900

    応援メッセージのテスト追加

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -2,18 +2,13 @@ import { render, screen } from '@testing-library/react'
 import Home from '../pages/index'
 import '@testing-library/jest-dom'
 
-jest.mock('../pages/index', () => {
-  const originalModule = jest.requireActual('../pages/index');
-  return {
-    __esModule: true,
-    ...originalModule,
-    getDogImage: jest.fn(),
-    randomPickMessage: jest.fn().mockReturnValue('今日も一日がんばるワン！')
-  }
-})
-
 describe('Home', () => {
-  it('renders a title', () => {
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  test('応援メッセージが表示されること', () => {
+    jest.spyOn(global.Math, 'random').mockReturnValue(0);
     render(<Home />)
     expect(screen.getByText('今日も一日がんばるワン！')).toBeInTheDocument()
   })
